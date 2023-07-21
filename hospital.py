70f04ef0f98018aea84fd92f072c09ab3f6baa0a
Tan Bao Tin Nguyen <93857461+tintokaka@users.noreply.github.com>
2023-07-21 12:36:50 -0700

    Add files via upload

---
+++ hospital.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from tkinter import ttk
+import random
+import time
+import datetime
+from tkinter import messagebox
+import mysql.connector
+
+class Hospital:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("HOspital Management System")
+        self.root.geometry()
